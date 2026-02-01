f69f87d3d311e11d97a59006052c82e13c0e296d
kaysarsoft <1290361724@qq.com>
2026-02-01 18:50:08 +0800

    Update blog content and dependencies

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,16 @@
 export default {
   // 网站标题
-  Title: '凯撒博客',
+  Title: '韩小韩博客',
   // 网站地址
-  Site: 'https://blog.kaysar.top/',
+  Site: 'https://www.vvhan.com',
   // 网站副标题
   Subtitle: '不曾与你分享的时间,我在进步.',
   // 网站描述
-  Description: '凯撒博客 专注于前开发与相关技术的实战分享，涵盖Vue框架、Node.js、Serverless等，并涉及Node、Python、Linux、Docker等领域。同时，博客也分享作者的生活、音乐和旅行的热爱。',
+  Description: '韩小韩博客 专注于前开发与相关技术的实战分享，涵盖Vue框架、Node.js、Serverless等，并涉及Node、Python、Linux、Docker等领域。同时，博客也分享作者的生活、音乐和旅行的热爱。',
   // 网站作者
-  Author: '.凯撒',
+  Author: '.𝙃𝙖𝙣',
   // 作者头像
-  Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=1290361724&s=640',
+  Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=1655466387&s=640',
   // 网站座右铭
   Motto: '运气是计划之外的东西.',
   // Cover 网站缩略图
@@ -58,13 +58,17 @@ export default {
     { text: '昔日', link: '/archives', icon: 'Nav_archives' },
     { text: '留言', link: '/message', icon: 'Nav_message' },
     { text: '关于', link: '/about', icon: 'Nav_about' },
-    { text: '首页', link: 'https://www.kaysar.topwww.kaysar.top/', target: true, icon: 'Nav_link' },
+    { text: 'API', link: 'https://api.vvhan.com/', target: true, icon: 'Nav_link' },
   ],
   // 侧边栏个人网站
   WebSites: [
     // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可 <不需要文件后缀名>（封装了 SVG 组件 为了极致压缩 SVG）
     // 建议使用 https://tabler.io/icons 直接下载 SVG
-    { text: 'Github', link: 'https://www.kaysar.top/', target: true, icon: 'WebSite_home' },
+    { text: 'Github', link: 'https://github.com/uxiaohan', icon: 'WebSite_github' },
+    { text: '韩小韩API', link: 'https://api.vvhan.com', icon: 'WebSite_api' },
+    { text: '每日热榜', link: 'https://hot.vvhan.com', icon: 'WebSite_hot' },
+    { text: '骤雨重山图床', link: 'https://wp-cdn.4ce.cn', icon: 'WebSite_img' },
+    { text: 'HanAnalytics', link: 'https://analytics.vvhan.com', icon: 'WebSite_analytics' },
   ],
   // 侧边栏展示
   AsideShow: {
@@ -102,15 +106,15 @@ export default {
     }
   },
   // Han Analytics 统计（https://github.com/uxiaohan/HanAnalytics）
-  // HanAnalytics: { enable: true, server: 'https://analytics.vvhan.com', siteId: 'Hello-HanHexoBlog' },
+  HanAnalytics: { enable: true, server: 'https://analytics.vvhan.com', siteId: 'Hello-HanHexoBlog' },
   // Google 广告
-  // GoogleAds: {
-  //   ad_Client: '', //ca-pub-xxxxxx
-  //   // 侧边栏广告(不填不开启)
-  //   asideAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`,
-  //   // 文章页广告(不填不开启)
-  //   articleAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`
-  // },
+  GoogleAds: {
+    ad_Client: '', //ca-pub-xxxxxx
+    // 侧边栏广告(不填不开启)
+    asideAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`,
+    // 文章页广告(不填不开启)
+    articleAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`
+  },
   // 文章内赞赏码
   Reward: {
     // 支付宝收款码
